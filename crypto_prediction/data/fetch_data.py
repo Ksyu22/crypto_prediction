fefad227537d5a0118348028c77f8be1b33cb1bb
Oksana Mishchenko <ksyu.ha@hotmail.com>
2023-02-04 16:39:30 +0100

    replacing nan values

--- crypto_prediction/data/fetch_data.py
+++ crypto_prediction/data/fetch_data.py
@@ -1,6 +1,7 @@
 import requests
 
 import pandas as pd
+import numpy as np
 from datetime import date, datetime
 
 import yfinance as yf
@@ -17,11 +18,11 @@ def fetch_fng_index(limit=100000) -> pd.DataFrame:
             }
     response = requests.get(url, params).json()
 
-    fng_df = pd.DataFrame(response['data']).drop(columns='time_until_update')
+    fng_df = pd.DataFrame(response['data']).drop(columns=['time_until_update', 'value_classification'])
 
     fng_df.timestamp = pd.to_datetime(fng_df.timestamp, format='%d-%m-%Y')
     fng_df.set_index('timestamp', inplace=True)
-    fng_df.rename(columns={'value': 'fng_idx', 'value_classification': 'fng_class'}, inplace=True)
+    fng_df.rename(columns={'value': 'fng_idx'}, inplace=True)
 
     fng_df.fng_idx = fng_df.fng_idx.astype('float')
 
@@ -57,12 +58,29 @@ def fetch_rate_exchange(ticker: str, limit=100000) -> pd.DataFrame:
     return df_rate
 
 def fetch_data(ticker: str, limit=100000) -> pd.DataFrame:
-    '''Funcrtion returns df containing fear and greed index + change rate
+    '''Function returns df containing fear and greed index + change rate
     for a specific ticker.'''
     fng_df = fetch_fng_index(limit)
     rate_df = fetch_rate_exchange(ticker, limit)
 
-    return fng_df.merge(rate_df, on='timestamp')
+    fng_df = fng_df.merge(rate_df, on='timestamp', how='outer').sort_index(ascending=False)
+    # ealing with nan values
+    # nan values for fng index
+    nan_dates = fng_df[fng_df.fng_idx.isnull()]
+
+    # the first and last date with nan value
+    first_nan_idx = datetime.strftime(nan_dates.index[0], format='%Y-%m-%d')
+    last_nan_idx = datetime.strftime(nan_dates.index[-1], format='%Y-%m-%d')
+
+    # index of the first and last date with nan value
+    idx_first = fng_df.index.get_loc(first_nan_idx)
+    idx_last = fng_df.index.get_loc(last_nan_idx)
+
+    mean_nan = int(fng_df.fng_idx[idx_first-3:idx_last+4].mean())
+
+    fng_df.fng_idx = fng_df.fng_idx.replace(np.nan, mean_nan)
+
+    return fng_df
 
 
 if __name__ == '__main__':
